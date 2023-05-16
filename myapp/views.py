5a90ce30881ea435d62bc0240980f98aa1fb126c
Tec94 <caodangkhiet1@gmail.com>
2023-05-16 15:34:23 -0500

    add login view in views.py add 3rd party comment seperator in settings.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from myapp.models import Users
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def login(request):
+    request.GET.get('name')
+    return render(request, 'login.html', {})
+
 def home(request):
     return render(request, 'home.html', {})
 
