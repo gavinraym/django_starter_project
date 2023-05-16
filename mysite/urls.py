212ef940b067282aa03c0a4bea7465521ebb3c1d
Tec94 <caodangkhiet1@gmail.com>
2023-05-16 15:33:05 -0500

    add path for home.html after replacing first path to login.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login, name='login'),
+    path('home', views.home, name='home'),
     path('contact', views.contact, name='contact'),
     path('about', views.about, name='about'),
 ]
